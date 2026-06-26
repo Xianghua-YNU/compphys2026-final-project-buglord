73d5c731e63b218d0a15810dc11ed4d76489a681
fuX516 <2243431928@qq.com>
2026-06-26 10:52:28 +0800

    Update analysis.py

--- 2_Code/analysis.py
+++ 2_Code/analysis.py
@@ -37,10 +37,11 @@ def plot_mu_z(z, mu_obs, mu_err, samples, filename="mu_fit.png", n_models=100):
 
     plt.figure(figsize=(8, 6))
     # plot posterior predictive curves
-   Om, Ol = samples[i]
-   mu0 = 0.3
-        mu_model = distance_modulus(zgrid, Om) + mu0
-        plt.plot(zgrid, mu_model, color="C0", alpha=0.05)
+for i in range(len(samples)):
+    Om, Ol = samples[i]
+    mu0 = 0.3
+    mu_model = distance_modulus(zgrid, Om) + mu0
+    plt.plot(zgrid, mu_model, color="C0", alpha=0.05)
 
     # plot best-fit median curve
     med = np.median(samples, axis=0)
